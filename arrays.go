24eaf7abe7857d0381e31a542c9ca45319539c16
lep_13 <lepakshi1304@gmail.com>
2024-05-17 13:07:11 +0530

    arrays delcration methods, length and type

---
+++ arrays.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare and initialize an array using the shorthand syntax
+	greetings := [2]string{"Bonjour", "Hola"}
+	fmt.Println(greetings[0], greetings[1]) // Output individual elements
+	fmt.Println(greetings)                  // Output the entire array
+
+	// Using a composite literal to declare and initialize an array
+	fibonacci := [...]int{0, 1, 1, 2, 3, 5, 8}
+	fmt.Println(fibonacci)
+
+	// Declare an array with predefined size and then initialize each element separately
+	var cities [3]string
+	cities[0] = "Tokyo"
+	cities[1] = "Paris"
+	cities[2] = "New York"
+	fmt.Println(cities)
+}
+
+
+// import (
+// 	"fmt"
+
+// 	"github.com/brianvoe/gofakeit/v6"
+// )
+
+// func main() {
+
+// 	size := 10
+// 	arr := make([]string, size)
+// 	for i := range arr {
+// 		arr[i] = gofakeit.Word()
+// 	}
+
+// 	fmt.Println("Array:", arr)
+// 	fmt.Println("Length:", len(arr))
+// 	fmt.Printf("Type: %T\n", arr)
+// }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/m/v2
+
+go 1.22.2
+
+require github.com/brianvoe/gofakeit/v6 v6.28.0 // indirect
